70d030fd0dfad770ceb0adacf363e2056cf736cb
ravichandran <ravi926k@gmail.com>
2021-09-22 19:43:13 +0530

    Commit3

--- src/main/java/pageobjects/LoginPageObjects.java
+++ src/main/java/pageobjects/LoginPageObjects.java
@@ -1,12 +1,17 @@
 package pageobjects;
 
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPageObjects {
+import resources.BaseClass;
+
+public class LoginPageObjects extends BaseClass {
 
 	public WebDriver driver;
+	public Properties prop;
 
 	@FindBy(xpath = "//a[@class='theme-btn']")
 	WebElement loginbutton;
@@ -20,15 +25,17 @@ public class LoginPageObjects {
 	@FindBy(xpath = "//input[@class='btn btn-primary btn-md login-button']")
 	WebElement submit;
 
-	
 	public LoginPageObjects(WebDriver driver) {
-		
-		this.driver=driver;
+
+		this.driver = driver;
 	}
-	
-	
+
+	public WebElement loginbutton() {
+		loginbutton.click();
+		email.sendKeys(prop.getProperty("username"));
+		password.sendKeys(prop.getProperty("pass"));
+		submit.click();
+		return loginbutton;
+	}
+
 }
-
-
-
-
