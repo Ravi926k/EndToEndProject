b3cd9f16954a35019763464f1ebe37214b223fcd
ravichandran <ravismart926@gmail.com>
2021-09-28 20:53:08 +0530

    latestcommit1

--- src/main/java/pageobjects/ExtentReportandListeners.java
+++ src/main/java/pageobjects/ExtentReportandListeners.java
@@ -9,39 +9,45 @@ import com.aventstack.extentreports.ExtentTest;
 import resources.BaseClass;
 import resources.ExtentReportClass;
 
-
-
 public class ExtentReportandListeners extends BaseClass implements ITestListener {
 	ExtentTest test;
-	
-	ExtentReports extents=ExtentReportClass.getextentreport();
-	ThreadLocal<ExtentTest> extenttest=new ThreadLocal<ExtentTest>();
+
+	ExtentReports extents = ExtentReportClass.getextentreport();
+	ThreadLocal<ExtentTest> extenttest = new ThreadLocal<ExtentTest>();
+
 	public void onTestStart(ITestResult result) {
-		// TODO Auto-generated method stub
 		
+		test=extents.createTest(result.getMethod().getMethodName());
+
 	}
+
 	public void onTestSuccess(ITestResult result) {
-		// TODO Auto-generated method stub
 		
+
 	}
+
 	public void onTestFailure(ITestResult result) {
-		// TODO Auto-generated method stub
 		
+
 	}
+
 	public void onTestSkipped(ITestResult result) {
-		// TODO Auto-generated method stub
 		
+
 	}
+
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
 		
+
 	}
+
 	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
 		
+
 	}
+
 	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
 		
+
 	}
 }
